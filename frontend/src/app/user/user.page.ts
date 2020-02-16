f9c977f1ead4e91f3261747b73bebb1748e8df15
MAC RoboMaster <45277712+macrobomaster@users.noreply.github.com>
2020-02-16 04:33:19 -0500

    wrapping up user profile

--- frontend/src/app/user/user.page.ts
+++ frontend/src/app/user/user.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPage implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
+  home = this.userService.currentUserHome;
+  work = this.userService.currentUserWork;
+  editHome = false;
+  editWork = false;
 
   ngOnInit() {
+  }
+
+  saveAddress() {
+    this.userService.changeHomeAddress(this.home).subscribe(res => { });
+    this.userService.changeWorkAddress(this.work).subscribe(res => { });
+    this.router.navigateByUrl('/');
   }
 
 }
