7980f785d109c6f04ec6d2420a5b30741b775676
Jarvis Wang <wangj230@mcmaster.ca>
2020-02-16 01:25:51 -0500

    generated page user

--- frontend/src/app/services/search.service.ts
+++ frontend/src/app/services/search.service.ts
@@ -6,6 +6,79 @@ import { Observable } from 'rxjs';
 })
 export class SearchService {
 
+  shoppingList = [{
+    itemName: 'i9-9900k',
+    store: 'Canada Computers',
+    location: '1306 Kennedy Rd, Scarborough, ON M1P 2L5',
+    price: 800,
+    description: 'cpu',
+},
+{
+    itemName: 'RTX 2080ti',
+    store: 'Best Buy',
+    price: 1300,
+    decription: 'gpu',
+},
+{
+    itemName: 'GTX 1660',
+    store: 'Canada Computers',
+    location: '1306 Kennedy Rd, Scarborough, ON M1P 2L5',
+    price: 350,
+    description: 'gpu',
+},
+{
+    itemName: 'G-Skill Ripjaws 16gb 3200mHz kit',
+    store: 'Best Buy',
+    price: 72,
+    description: 'memory',
+},
+{
+    itemName: 'Deepcool Matrexx 50 rgb',
+    store: 'Canada Computers',
+    location: '1306 Kennedy Rd, Scarborough, ON M1P 2L5',
+    price: 85,
+    description: 'computer case',
+},
+{
+    itemName: '512gb ssd',
+    store: 'Canada Comptuers',
+    location: '1306 Kennedy Rd, Scarborough, ON M1P 2L5',
+    price: 92,
+    description: 'Storage',
+},
+{
+    itemName: '256gb ssd',
+    store: 'Best Buy',
+    price: 40,
+    description: 'Storage',
+},
+{
+    itemName: 'Ryzen 5 3600',
+    store: 'Canada Computers',
+    location: '1306 Kennedy Rd, Scarborough, ON M1P 2L5',
+    price: 240,
+    description: 'cpu',
+},
+{
+    itemName: 'Ryzen 7 3700x',
+    store: 'Best Buy',
+    location: '695 Wilson Ave, Toronto, ON M3K 1E3',
+    price: 450,
+    description: 'cpu'
+},
+{
+    itemName: '1tb WD blue hdd',
+    store: 'Best Buy',
+    location: '695 Wilson Ave, Toronto, ON M3K 1E3',
+    price: 50,
+    description: 'storage',
+},
+{
+    itemName: 'NZXT H500i',
+    store: 'Canada Comptuters',
+    price: 100,
+    description: 'computer case',
+}];
   constructor(private http: HttpclientService) { }
 
   searchItems(value: string): Observable<any[]> {
--- frontend/src/app/tab3/tab3.page.ts
+++ frontend/src/app/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { SearchService } from '../services/search.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-tab3',
@@ -13,34 +14,91 @@ export class Tab3Page implements AfterViewInit {
   directionsService = new google.maps.DirectionsService();
   directionsDisplay = new google.maps.DirectionsRenderer();
   directionForm: FormGroup;
+
+  navigationLink: string;
+  wayPointsLink = '';
+
+  locations = [
+    {
+      name: 'i7-8700k',
+      location: '20 Baffin Court, Richmond Hill, ON',
+      price: '$700'
+    },
+    {
+      name: 'i8-8800k',
+      location: '45 Red Maple Rd, Richmond Hill, ON L4B 4M6',
+      price: '$600'
+    },
+    {
+      name: 'i9-8900k',
+      location: '6364 Yonge St, North York, ON M2M 3X4',
+      price: '$670'
+    },
+    {
+      name: 'i10-81000k',
+      location: '1265 Military Trail, Scarborough, ON M1C 1A4',
+      price: '$89000'
+
+    }
+  ];
   constructor(private fb: FormBuilder) {
-    this.createDirectionForm();
+    // this.createDirectionForm();
   }
 
-  createDirectionForm() {
-    this.directionForm = this.fb.group({
-      source: ['Markham', Validators.required],
-      destination: ['Toronto', Validators.required]
-    });
-  }
+  // createDirectionForm() {
+  //   this.directionForm = this.fb.group({
+  //     source: ['Markham', Validators.required],
+  //     destination: ['Toronto', Validators.required]
+  //   });
+  // }
 
   ngAfterViewInit(): void {
-    const map = new google.maps.Map(this.mapNativeElement.nativeElement, {
-      zoom: 7,
-      center: {lat: 41.85, lng: -87.65}
-    });
-    this.directionsDisplay.setMap(map);
+
+    this.calculateAndDisplayRoute();
+
   }
 
-  calculateAndDisplayRoute(formValues) {
+  calculateAndDisplayRoute() {
+    const map = new google.maps.Map(this.mapNativeElement.nativeElement, {
+      zoom: 7,
+      center: { lat: 41.85, lng: -87.65 }
+    });
+    this.directionsDisplay.setMap(map);
     const that = this;
+    const wayPoints = [];
+    console.log(this.locations);
+    this.locations.forEach(item => {
+      wayPoints.push({ location: item.location });
+    });
+    wayPoints.splice(0, 1);
+    wayPoints.splice(wayPoints.length - 1, 1);
+
     this.directionsService.route({
-      origin: formValues.source,
-      destination: formValues.destination,
+      // origin: formValues.source,
+      // destination: formValues.destination,
+
+      origin: this.locations[0].location,
+      destination: this.locations[this.locations.length - 1].location,
+      waypoints: wayPoints,
       travelMode: google.maps.TravelMode.DRIVING
     }, (response, status) => {
+      console.log(response);
+      console.log(status);
+      console.log(wayPoints);
+      wayPoints.forEach(point => {
+        this.wayPointsLink += (point.location + '%7C');
+      });
+
+      console.log(this.wayPointsLink);
       if (status === 'OK') {
         that.directionsDisplay.setDirections(response);
+        this.navigationLink = 'https://www.google.com/maps/dir/?api=1&origin=' + this.locations[0].location +
+        '&destination=' + this.locations[this.locations.length - 1].location +
+        '&travelmode=driving' +
+        '&waypoints=' + this.wayPointsLink.replace(/\s/g, '+');
+        this.navigationLink = this.navigationLink.slice(0, -3).replace(/\s/g, '+');
+        console.log(this.navigationLink);
+
       } else {
         window.alert('Directions request failed due to ' + status);
       }
